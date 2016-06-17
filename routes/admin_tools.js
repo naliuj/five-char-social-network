b10f6bab0b146ecf8ec8013e55ef6917b37e752b
Julian Rose <julianrose23@hotmail.com>
2016-06-17 08:30:17 -0400

    it is now possible to unlock a  user

--- routes/admin_tools.js
+++ routes/admin_tools.js
@@ -8,7 +8,24 @@ module.exports = function(app, passport) {
         // Find the user
         User.findById(req.body.lock, function(err, user) {
             if (err) return console.error(err);
+            // Lock the user
             user.locked = true;
+            // Save the changes
+            user.save(function(err) {
+                if (err) return console.error(err);
+            });
+        });
+        res.redirect('back');
+    });
+
+    // Route for unlocking users
+    app.post('/unlock', isAdmin, function(req, res) {
+        // Find the user
+        User.findById(req.body.unlock, function(err, user) {
+            if (err) return console.error(err);
+            // Unlock the user
+            user.locked = false;
+            // Save the changes
             user.save(function(err) {
                 if (err) return console.error(err);
             });
